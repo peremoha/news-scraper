04a92e24d96666bb9897c0d34cbdcbf71cddde27
Roman <dvtytrhenbqrjvg@gmail.com>
2025-05-04 21:27:36 +0300

    Setup Apollo Client and root configuration in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./styles.css"; // Стилі
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-// Налаштування Apollo Client
+// Налаштування GraphQL API URL
+const httpLink = createHttpLink({
+  uri: process.env.REACT_APP_GRAPHQL_URL || "http://127.0.0.1:5000/graphql",
+});
+
+// Додавання токена до заголовку Authorization
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "", // Додавання токена
+    },
+  };
+});
+
+// Apollo Client з авторизацією
 const client = new ApolloClient({
-  uri: process.env.REACT_APP_GRAPHQL_URL, // URL із .env
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
