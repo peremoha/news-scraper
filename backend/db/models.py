1c5cb0bdf6c8107f667dae360029c3b9bd1c65a3
Roman <dvtytrhenbqrjvg@gmail.com>
2025-05-04 18:00:41 +0300

    Add database configuration and models

---
+++ backend/db/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+from db.database import engine
+
+Base = declarative_base()
+
+class News(Base):
+    __tablename__ = "news"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    title = Column(String, nullable=False)
+    body = Column(String, nullable=True)
+    publication_date = Column(String, nullable=False)
+    source_url = Column(String, nullable=False)
+
+Base.metadata.create_all(engine)
