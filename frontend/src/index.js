358b0b1926ecdb22aaf8e68af4f2069c9224f030
Roman <dvtytrhenbqrjvg@gmail.com>
2025-05-04 18:23:32 +0300

    Add Apollo Client for GraphQL integration

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css"; // Стилі
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Налаштування Apollo Client
+const client = new ApolloClient({
+  uri: process.env.REACT_APP_GRAPHQL_URL, // URL із .env
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
