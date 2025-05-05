81d392c8011ffcfbaab6430df6e1b69c48594d46
Roman <dvtytrhenbqrjvg@gmail.com>
2025-05-05 13:36:48 +0300

    transferred component to folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import NewsList from "./components/NewsList";
+import NewsList from "./components/NewsList/NewsList";
 import { isAuthenticated } from "./utils/auth";
 
 const PrivateRoute = ({ children }) => {
